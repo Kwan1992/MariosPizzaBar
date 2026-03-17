0f6a9d1bec89b03d634dde03b263a47913409741
Linuskramshoej <linus.kramshoej@protonmail.com>
2026-03-17 11:52:51 +0100

    added pizza enums

--- src/model/Pizzas.java
+++ src/model/Pizzas.java
@@ -1,5 +1,18 @@
 package model;
 
 public enum Pizzas {
-
+    VESUVIO,
+    AMERIKANER,
+    CACCIATORE,
+    CARBONA,
+    DENNIS,
+    BERTIL,
+    SILVIA,
+    VICTORIA,
+    TORONFO,
+    CAPPRICIOSA,
+    HAWAII,
+    LE_BLISSOLA,
+    VENEZIA,
+    MAFIA
 }
