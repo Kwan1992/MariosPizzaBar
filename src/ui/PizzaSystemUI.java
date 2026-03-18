a83167eafc7f4aebcfe5d051cb807a6922dc2583
SimonM32 <simi1002@stud.ek.dk>
2026-03-18 11:15:25 +0100

    Lavede while loop og switch cases i ui for terminal system

--- src/ui/PizzaSystemUI.java
+++ src/ui/PizzaSystemUI.java
@@ -1,8 +1,15 @@
 package ui;
 
+import java.util.Scanner;
+
 public class PizzaSystemUI {
 
+    private Scanner scanner;
 
+    public UserInterface() {
+
+        scanner = new Scanner(System.in);
+    }
 
 
 
@@ -15,7 +22,30 @@ public class PizzaSystemUI {
 
             System.out.println("\nPizzaBar System");
             System.out.println("1. Vis menukort");
-            System.out.println("2. ")
+            System.out.println("2. Opret Order");
+            System.out.println("3. Vis liste over aktive bestillinger");
+            System.out.println("4. Fjern og gem order");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+            }
 
         }
 
