37167f5a4e90e367d6c57a9f67a44c292413865d
Linuskramshoej <linus.kramshoej@protonmail.com>
2026-03-18 11:06:20 +0100

    created pizza class

--- src/model/Pizza.java
+++ src/model/Pizza.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Pizza {
+
+    private int number;
+    private PizzaType pizzaType;
+    private double price;
+
+    public Pizza(int number, PizzaType pizzaType, double price) {
+        this.number = number;
+        this.pizzaType = pizzaType;
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public PizzaType getPizzaType() {
+        return pizzaType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza number: " + number + "\nPizza type: " + pizzaType + "\nPizza price: " + price;
+    }
 }
